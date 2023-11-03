e93f98300789d6402ed47e0821250ffc3f045fc1
Mauro Jimenez <maurojim123@gmail.com>
2023-11-03 16:23:45 -0300

    raza crud

---
+++ src/raza/raza.controller.ts
@@ -0,0 +1,97 @@
+import { Request, Response, NextFunction } from "express";
+import { Raza } from "./raza.entity.js";
+import { orm } from "../shared/db/orm.js";
+
+const em= orm.em;
+em.getRepository(Raza);
+
+async function sanitizeRazaInput(req:Request, res:Response, next:NextFunction){
+    req.body.sanitizedInput = {         
+        descripcion: req.body.descripcion,
+        especie: req.body.especie,
+    }
+    Object.keys(req.body.sanitizedInput).forEach(key => {
+        if(req.body.sanitizedInput[key] === undefined){
+            delete req.body.sanitizedInput[key]
+        }
+    } )
+    next();
+}
+
+async function findAll(req:Request, res:Response){
+    try {
+        const raza = await em.find(Raza, {},{populate:['especie']})
+        res.status(200).json({
+            message: 'Razas encontradas',
+            data:raza
+        });
+    } catch (error:any) {
+        res.status(500).json({
+            message:error.message
+        })
+    }
+};
+
+async function findOne(req:Request, res:Response ){
+    try {
+        const id = req.params.id;
+        const raza = await em.findOneOrFail(Raza, {id},{populate:['especie']});
+        res.status(200).json({
+            message:'Raza encontrada',
+            data:raza
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+};
+
+async function add(req:Request, res:Response){
+    try {
+        const newRaza = em.create(Raza, req.body.sanitizedInput)
+        await em.flush();
+        return res.status(201).json({
+            message:'Raza creada',
+            data: newRaza
+        });
+    } catch (error:any) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+};
+
+async function update(req:Request, res:Response){
+    try {
+        const id = req.params.id;
+        const raza = await em.findOneOrFail(Raza, {id})
+        em.assign(raza, req.body.sanitizedInput)
+        await em.flush();
+        res.status(200).send({
+            message: 'Raza actualizada correctamente',
+            data: raza
+        });
+    } catch (error:any) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+};
+
+async function remove(req:Request, res:Response){
+    try {
+        const id = req.params.id;
+        const razaToDelete = await em.getReference(Raza, id);
+        em.removeAndFlush(razaToDelete)
+        res.status(200).json({
+            message: 'raza eliminada correctamente'
+        })
+    } catch (error:any) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+};
+
+export {sanitizeRazaInput, findAll, findOne, add, update, remove}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { veterinaryRouter } from "./veterinario/veterinario.routes.js";
 import { EspecieRouter } from "./especie/especie.routes.js";
 import { rolRouter } from "./rol/rol.routes.js";
 import { practicaRouter } from "./practica/practica.routes.js";
+import { razaRouter } from "./raza/raza.routes.js";
 
 const router = express.Router();
 
@@ -12,7 +13,8 @@ router
     .use('/veterinarios', veterinaryRouter)
     .use('/especies', EspecieRouter)
     .use('/roles', rolRouter)
-    .use('/practicas', practicaRouter);
+    .use('/practicas', practicaRouter)
+    .use('/raza', razaRouter);
     
 
 export {router as indexRouter}
