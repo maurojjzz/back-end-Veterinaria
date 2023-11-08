f3ef98122efb9a9e6fc3651c87ad948fcf892c7f
Mauro Jimenez <maurojim123@gmail.com>
2023-11-08 16:28:14 -0300

    minimum Changes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 app.listen(process.env.PORT, ()=>{
-    console.log('Server running on port http://localhost:3000/');
+    console.log(`Server running on port http://localhost:${process.env.PORT}/`);
 });
 
